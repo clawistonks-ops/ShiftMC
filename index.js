b8b090183723c72351a47a828ad203b30b563f0a
clawistonks-ops <clawistonks@gmail.com>
2025-08-28 02:52:51 +0200

    index.js

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// SE usi React Router:
+import { HashRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
